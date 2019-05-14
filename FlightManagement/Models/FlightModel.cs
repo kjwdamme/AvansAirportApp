ed5d31c0e7ad945049f91adfd91cab3e0ddf475f
unknown <kjwdamme@avans.nl>
2019-05-14 20:07:07 +0200

    Added Model classes

---
+++ FlightManagement/Models/FlightModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightManagement.Models
+{
+    public class FlightModel
+    {
+        public int Id { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public TimeSpan Delay { get; set; }
+        public string Destination { get; set; }
+        public bool IsArriving { get; set; }
+        public AirlineModel AirlineModel { get; set; }
+        public int PlaneId { get; set; }
+        public PlaneModel PlaneModel { get; set; }
+    }
+}
