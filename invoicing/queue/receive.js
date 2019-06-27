cac05b9d29c87e632eb0776ba6ccdcbe8028df56
Mike Gomper <mgomper@avans.nl>
2019-06-27 12:43:13 +0200

    publish subscribe queue

--- invoicing/queue/receive.js
+++ invoicing/queue/receive.js
@@ -3,68 +3,73 @@ Invoice = require('../models/commandModel');
 var commandController = require('../controllers/commandController');
 var mongoose = require('mongoose');
 
-    module.exports = {
-        receive: function () {
-            amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
-                if (error0) {
-                    throw error0;
+module.exports = {
+    receive: function () {
+        amqp.connect('amqp://localhost', function (error0, connection) {
+            if (error0) {
+                throw error0;
+            }
+            connection.createChannel(function (error1, channel) {
+                if (error1) {
+                    throw error1;
                 }
-                connection.createChannel(function (error1, channel) {
-                    if (error1) {
-                        throw error1;
+                var exchange = 'testbookings';
+
+                channel.assertExchange(exchange, 'fanout', {
+                    durable: false
+                });
+
+                channel.assertQueue('bookingqueue', {
+                    exclusive: false
+                }, function (error2, q) {
+                    if (error2) {
+                        throw error2;
                     }
-            
-                    var queue = 'bookings2';
-            
-                    channel.assertQueue(queue, {
-                        durable: true 
-                    });
+                    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+                    channel.bindQueue('bookingqueue', exchange, '');
 
-                    channel.prefetch(1);
-            
-                    console.log(" [*] Waiting for messages in %s.", queue);
-            
-                    channel.consume(queue, function (msg) {
-                        console.log(" [x] Received message...");
+                    channel.consume('bookingqueue', function (msg) {
+                        if (msg.content) {
+                            mongoose.connect('mongodb://admin:Admin0@ds034797.mlab.com:34797/writeinvoice', { useNewUrlParser: true });
 
-                        mongoose.connect('mongodb://admin:Admin0@ds034797.mlab.com:34797/writeinvoice', { useNewUrlParser: true });
+                            var db = mongoose.connection;
 
-                        var db = mongoose.connection;
+                            var booking = JSON.parse(msg.content.toString());
 
-                        var booking = JSON.parse(msg.content.toString());
-                    
-                        db.once('open', function callback() {
-                            var invoice = new Invoice()
-            
-                            invoice._id = booking._id;
-                            invoice.flightID = booking.flightID;
-                            invoice.passenger = booking.passenger;
-                            invoice.email = booking.email;
-                            invoice.costs = booking.costs;
-                
-                            console.log(invoice);
-                    
-                            invoice.save(function (err) {
-                                if (err) return console.error(err);
-                    
-                                commandController.sendInvoice(invoice);
+                            db.once('open', function callback() {
+                                var invoice = new Invoice()
 
-                                mongoose.connection.close();
-                                mongoose.disconnect();
+                                invoice._id = booking._id;
+                                invoice.flightID = booking.flightID;
+                                invoice.passenger = booking.passenger;
+                                invoice.email = booking.email;
+                                invoice.costs = booking.costs;
 
-                                var secs = msg.content.toString().split('.').length - 1;
+                                console.log(invoice);
 
-                                setTimeout(function() {
-                                    console.log(" [x] Done");
-                                    channel.ack(msg);
-                                  }, secs * 1000);
-                    
+                                invoice.save(function (err) {
+                                    if (err) return console.error(err);
+
+                                    commandController.sendInvoice(invoice);
+
+                                    mongoose.connection.close();
+                                    mongoose.disconnect();
+
+                                    var secs = msg.content.toString().split('.').length - 1;
+
+                                    setTimeout(function () {
+                                        console.log(" [x] Done");
+                                        channel.ack(msg);
+                                    }, secs * 1000);
+
+                                });
                             });
-                        });     
+                        }
                     }, {
                             noAck: false
                         });
                 });
             });
-          }
-          };
+        }); 
+    }
+}
